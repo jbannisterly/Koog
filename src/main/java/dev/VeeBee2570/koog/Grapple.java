e976e36953f433fb79d6ded9e79675737f0284b0
jbannisterly <jbannisterly@gmail.com>
2026-07-24 17:27:36 +0100

    Get grapple position

--- src/main/java/dev/VeeBee2570/koog/Grapple.java
+++ src/main/java/dev/VeeBee2570/koog/Grapple.java
@@ -1,11 +1,69 @@
 package dev.VeeBee2570.koog;
 
+import org.apache.commons.compress.archivers.zip.PKWareExtraHeader.HashAlgorithm;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class Grapple extends Item {
+
+    private static final float MaxWidth = 20;
 
     public Grapple(Properties properties) {
         super(properties);
     }
     
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+
+        if (!level.isClientSide()) {
+            Double blockDistance = Double.POSITIVE_INFINITY;
+            Double entityDistance = Double.POSITIVE_INFINITY;
+
+            Vec3 start = player.getEyePosition();
+            Vec3 end = player.getLookAngle().multiply(Grapple.MaxWidth, Grapple.MaxWidth, Grapple.MaxWidth).add(start);
+            HitResult blockCollision = level.clip(
+                new ClipContext(start, end, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player)   
+            );
+            HitResult entityCollision = ProjectileUtil.getEntityHitResult(
+                level, player, start, end, 
+                player.getBoundingBox().expandTowards(player.getLookAngle().scale(Grapple.MaxWidth + 2)), entity -> true
+            );
+            
+            if (blockCollision != null && blockCollision.getType() == HitResult.Type.BLOCK) {
+                blockDistance = blockCollision.distanceTo(player);
+            } 
+            if (entityCollision != null && entityCollision.getType() == HitResult.Type.ENTITY) {
+                entityDistance = entityCollision.distanceTo(player);
+            }
+
+            if (blockDistance != Double.POSITIVE_INFINITY || entityDistance != Double.POSITIVE_INFINITY) {
+                HitResult.Type collisionType;
+                Vec3 collisionPosition;
+
+                if (blockDistance < entityDistance) {
+                    collisionType = HitResult.Type.BLOCK;
+                    collisionPosition = blockCollision.getLocation();
+                } else {
+                    collisionType = HitResult.Type.ENTITY;
+                    collisionPosition = entityCollision.getLocation();
+                }
+
+                ExampleMod.LOGGER.info("grapple at " + collisionPosition.toString());
+            }
+        }
+        
+        return InteractionResultHolder.success(itemStack);
+    }
+
 }
